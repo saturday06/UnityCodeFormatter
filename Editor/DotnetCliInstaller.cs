69922db082e58d7552745303767fbda315906238
Isamu Mogi <isamu@leafytree.jp>
2024-09-30 01:18:19 +0900

    chore: manage http client

--- Editor/DotnetCliInstaller.cs
+++ Editor/DotnetCliInstaller.cs
@@ -2,20 +2,40 @@
 
 using System.Net.Http;
 using System.Threading.Tasks;
+using UnityEditor;
 using UnityEngine;
 
 namespace UnityCodeFormatter.Editor
 {
     public class DotnetCliInstaller
     {
-        private static readonly HttpClient s_httpClient = new();
+        // https://learn.microsoft.com/en-us/dotnet/fundamentals/networking/http/httpclient#create-an-httpclient
+        private static HttpClient? s_httpClient = new();
+
+        [InitializeOnLoadMethod]
+        public static void InitializeOnLoad()
+        {
+            // HttpClientはIDisposableだが使いまわすのが推奨されているのでstaticになっている。
+            // そのためアセンブリリロード直前にDisposeする。
+            AssemblyReloadEvents.beforeAssemblyReload += () =>
+            {
+                s_httpClient?.Dispose();
+                s_httpClient = null;
+            };
+        }
 
         public static async Task Install()
         {
-            HttpResponseMessage response = await s_httpClient.GetAsync(
+            HttpClient? httpClient = s_httpClient;
+            if (httpClient == null)
+            {
+                Debug.LogError("No http client");
+                return;
+            }
+            HttpResponseMessage response = await httpClient.GetAsync(
                 "https://dot.net/v1/dotnet-install.ps1"
             );
-            string script = await response.Content.ReadAsStringAsync();
+            byte[] script = await response.Content.ReadAsByteArrayAsync();
             Debug.LogFormat("{0}", script.Length);
         }
     }
