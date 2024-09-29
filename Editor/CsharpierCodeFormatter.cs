a390ada6be9a72e74736e570be07a879880a1fdd
Isamu Mogi <isamu@leafytree.jp>
2024-09-29 18:01:53 +0900

    chore: dotnet path

--- Editor/CsharpierCodeFormatter.cs
+++ Editor/CsharpierCodeFormatter.cs
@@ -1,13 +1,29 @@
+#nullable enable
+
+using UnityEditor;
+using System.IO;
 using UnityEngine;
 using UnityEditor.Experimental;
 
-namespace UnityCodeFormatter
+namespace UnityCodeFormatter.Editor
 {
     public class CsharpierCodeFormatter : AssetsModifiedProcessor
     {
+        [MenuItem("Tools/UnityCodeFormatter/Format")]
+        public static void Format() {
+        }
+            
         protected override void OnAssetsModified(string[] changedAssets, string[] addedAssets, string[] deletedAssets, AssetMoveInfo[] movedAssets)
         {
-            throw new System.NotImplementedException();
+#if UNITY_EDITOR_OSX
+            var dotnetPath = Path.Combine(
+                EditorApplication.applicationContentsPath,
+                "NetCoreRuntime",
+                "dotnet"
+            );
+            
+            Debug.LogFormat("dotnet: {0} {1}", dotnetPath, File.Exists(dotnetPath));
+#endif
         }
    }
 }
