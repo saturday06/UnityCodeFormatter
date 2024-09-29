9205e3ac4b93561a2e8950d38056af1a0af8587d
Isamu Mogi <isamu@leafytree.jp>
2024-09-29 23:58:38 +0900

    chore: installer script

--- Editor/DotnetCliInstaller.cs
+++ Editor/DotnetCliInstaller.cs
@@ -1,12 +1,22 @@
 #nullable enable
 
-using UnityEditor.Experimental;
+using System.Net.Http;
+using System.Threading.Tasks;
 using UnityEngine;
 
 namespace UnityCodeFormatter.Editor
 {
     public class DotnetCliInstaller
     {
-        public static void Install() { }
+        private static readonly HttpClient s_httpClient = new();
+
+        public static async Task Install()
+        {
+            HttpResponseMessage response = await s_httpClient.GetAsync(
+                "https://dot.net/v1/dotnet-install.ps1"
+            );
+            string script = await response.Content.ReadAsStringAsync();
+            Debug.LogFormat("{0}", script.Length);
+        }
     }
 }
